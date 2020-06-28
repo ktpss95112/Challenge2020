3d104ad3272a730964aad6ec365b02d2d32df97e
baluteshih <brian369999@gmail.com>
2020-06-28 14:08:45 +0800

    Add RAINBOW_GROUNDER

--- Model/GameObject/player.py
+++ Model/GameObject/player.py
@@ -104,5 +104,8 @@ class Player:
     
     def use_item(self):
         if self.keep_item_id == Const.INVINCIBLE_BATTERY :
-            self.radius *= 2 
+            self.radius *= 2
+            self.invincible_time = 5 * Const.FPS
+        elif self.keep_item_id == Const.RAINBOW_GROUNDER :
+            self.voltage -= 10
         self.keep_item_id = Const.NO_ITEM
