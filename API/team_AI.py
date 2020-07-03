a0c35320540850d3d771821b433725f8a0d418a1
lcagma <ck10500757@gmail.com>
2020-07-03 17:22:03 +0800

    modify team_AI.py  BTW  Can you defeat my AI?

--- API/team_AI.py
+++ API/team_AI.py
@@ -13,4 +13,9 @@ class TeamAI(BaseAI):
     def decide(self):
         my_pos = self.helper.get_self_position()
         radius = self.helper.get_self_radius()
-        return AI_DIR_LEFT
+        if my_pos[0] > 500:
+            return AI_DIR_LEFT
+        elif my_pos[0] < 300:
+            return AI_DIR_RIGHT
+        else:
+            return AI_DIR_JUMP 
