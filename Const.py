8e8200e282cad3bce76c68533693bf91def71bfc
chungen1007 <en1007en@gmail.com>
2020-07-03 18:23:27 +0800

    Modify constant

--- Const.py
+++ Const.py
@@ -1,16 +1,20 @@
 import pygame as pg
 import os.path
 
-# model
+# model-game
 FPS = 60 # frame per second
 GAME_LENGTH = 300 * FPS
+
+# model-player
+PLAYER_RADIUS = 20
+VALID_KO_TIME = 3 * FPS
+PLAYER_LIFE = 5
 PLAYER_INIT_POSITION = [
     pg.Vector2(100, 700),
     pg.Vector2(300, 700),
     pg.Vector2(500, 700),
     pg.Vector2(700, 700)
 ]
-PLAYER_RADIUS = 20
 PLAYER_INIT_VELOCITY = pg.Vector2(0, 0)
 PLAYER_NORMAL_SPEED = 150
 PLAYER_JUMP_SPEED = 1200
@@ -18,18 +22,24 @@ DIRECTION_TO_VEC2 = {
     'left': pg.Vector2(-1, 0),
     'right': pg.Vector2(1, 0),
 }
-PLATFORM_INIT_POSITION = [
-    [pg.Vector2(100, 730), pg.Vector2(700, 740)],
-    [pg.Vector2(100, 500), pg.Vector2(400, 510)]
-]
 PLAYER_JUMP_QUOTA = 3
-LIFE_BOUNDARY = pg.Rect(-700, -2000, 2200, 3500)
 PLAYER_RESPAWN_POSITION = [
     pg.Vector2(600, 200),
     pg.Vector2(600, 200),
     pg.Vector2(600, 200),
     pg.Vector2(600, 200)
 ]
+ATTACK_RADIUS = 3.5 * PLAYER_RADIUS
+VOLTAGE_INCREASE_CONST = 300
+
+# model-platform and boundary
+PLATFORM_INIT_POSITION = [
+    [pg.Vector2(100, 730), pg.Vector2(700, 740)],
+    [pg.Vector2(100, 500), pg.Vector2(400, 510)]
+]
+LIFE_BOUNDARY = pg.Rect(-700, -2000, 2200, 3500)
+
+# model-physics
 GRAVITY_ACCELERATION = 70 * FPS
 GRAVITY_ACCELERATION_FOR_ITEM = 40 * FPS
 DRAG_CRITICAL_SPEED = 464
@@ -38,30 +48,17 @@ VERTICAL_DRAG_EMERGE_SPEED = -1500
 ATTENUATION_COEFFICIENT = 0.5
 VERTICAL_SPEED_MINIMUM = 500
 HORIZONTAL_SPEED_MINIMUM = 20
-ATTACK_RADIUS = 3.5 * PLAYER_RADIUS
 BE_ATTACKED_ACCELERATION = 1200 * FPS
 BE_ATTACKED_MAX_ACCELERATION_DISTANCE = 20
-VOLTAGE_INCREASE_CONST = 300
-VALID_KO_TIME = 3 * FPS
-PLAYER_LIFE = 5
 
-# State machine constants
+# model-state machine constants
 STATE_POP = 0 # for convenience, not really a state which we can be in
 STATE_MENU = 1
 STATE_PLAY = 2
 STATE_STOP = 3
 STATE_ENDGAME = 4 # show score board and handle restart
 
-
-# view
-WINDOW_CAPTION = 'Challenge 2020'
-WINDOW_SIZE = (1200, 800)
-ARENA_SIZE = (800, 800)
-BACKGROUND_COLOR = pg.Color('black')
-PLAYER_COLOR = [pg.Color('green'), pg.Color('magenta'), pg.Color('orange'), pg.Color('red')]
-ITEM_COLOR = [pg.Color('white'), pg.Color('yellow'), pg.Color('deepskyblue'), pg.Color('gray'), pg.Color('mediumpurple'), pg.Color('darkgreen'), pg.Color('tan'), pg.Color('olivedrab')]
-
-# item
+# model-item
 ITEM_SPECIES = 7
 NO_ITEM = 0
 BANANA_PISTOL = 1
@@ -72,7 +69,13 @@ BANANA_PEEL = 5
 RAINBOW_GROUNDER = 6
 INVINCIBLE_BATTERY = 7
 
-# entities
+ITEMS_MAX_AMOUNT = 6
+ITEM_RADIUS = [7, 7, 7, 7, 7, 7, 7]
+ITEM_DRAG = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+ITEM_INIT_HEIGHT = 10
+ITEM_MAX_SPECIFIES = 7
+
+# model-entities
 BULLET_VELOCITY = 30 * FPS
 BULLET_RADIUS = 5
 BULLET_ATK = 10
@@ -93,11 +96,13 @@ BLACK_HOLE_GRAVITY_ACCELERATION = 500 * FPS
 
 ZAP_ZAP_ZAP_RANGE = 5
 
-ITEMS_MAX_AMOUNT = 6
-ITEM_RADIUS = [7, 7, 7, 7, 7, 7, 7]
-ITEM_DRAG = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-ITEM_INIT_HEIGHT = 10
-ITEM_MAX_SPECIFIES = 7
+# view
+WINDOW_CAPTION = 'Challenge 2020'
+WINDOW_SIZE = (1200, 800)
+ARENA_SIZE = (800, 800)
+BACKGROUND_COLOR = pg.Color('black')
+PLAYER_COLOR = [pg.Color('green'), pg.Color('magenta'), pg.Color('orange'), pg.Color('red')]
+ITEM_COLOR = [pg.Color('white'), pg.Color('yellow'), pg.Color('deepskyblue'), pg.Color('gray'), pg.Color('mediumpurple'), pg.Color('darkgreen'), pg.Color('tan'), pg.Color('olivedrab')]
 
 # controller
 PLAYER_MOVE_KEYS = {
