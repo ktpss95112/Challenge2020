ccddd9c9528dd6f2bc22561515edb644d0ba8593
ETT <b08902040@csie.ntu.edu.tw>
2020-07-05 21:35:37 +0800

    Add item_id in EventPlayerUseItem

--- Events/EventManager.py
+++ Events/EventManager.py
@@ -153,8 +153,9 @@ class EventPlayerItem(BaseEvent):
 class EventPlayerUseItem(BaseEvent):
     name = 'Player use item event (model => view)'
 
-    def __init__(self, player_id):
+    def __init__(self, player_id, item_id):
         self.player_id = player_id
+        self.item_id = item_id
 
     def __str__(self):
         return f'{self.name} => player_id {self.player_id} use item'
