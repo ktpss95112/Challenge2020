f4467971c28b352a9b3f6745364075a71e729f79
Henry <b08902142@ntu.edu.tw>
2020-07-12 16:27:17 +0800

    update henryAI

--- API/team_henryAI.py
+++ API/team_henryAI.py
@@ -25,6 +25,7 @@ class TeamAI(BaseAI):
     def __init__(self, helper):
         self.helper = helper
         self.enhancement = [0, 0, 0, 0]
+        self.searching_item = False
 
     def in_the_map(self, position):
         if position[1] < 603 + Const.Y_OFFSET and position[0] >= 0 + Const.X_OFFSET and position[0] <= 1132 + Const.X_OFFSET:
@@ -38,13 +39,14 @@ class TeamAI(BaseAI):
         if self.helper.get_other_voltage(self.helper.get_highest_voltage_player()) >= 100 and self.in_the_map(self.helper.get_other_position(self.helper.get_highest_voltage_player())):
             target_player_position = self.helper.get_other_position(self.helper.get_highest_voltage_player())
         if self.helper.get_distance(self_position , target_player_position) <= ATTACK_RADIUS and self.helper.get_self_can_attack_time() == 0:
+            self.searching_item = True
             return AI_DIR_ATTACK
         elif self.helper.get_self_keep_item_id() != NO_ITEM:
             return AI_DIR_USE_ITEM
-        elif self.in_the_map(target_player_position) and self.helper.get_self_can_attack_time() <= 0.5:
+        elif self.in_the_map(target_player_position) and self.helper.get_self_can_attack_time() <= 0.5 and self.searching_item:
             return self.helper.walk_to_position(target_player_position)
         elif self.in_the_map(nearest_item_position):
             return  self.helper.walk_to_position(nearest_item_position)
         
-        return self.helper.walk_to_position((200,0))
+        return self.helper.walk_to_position((590,0))
         # add your code
