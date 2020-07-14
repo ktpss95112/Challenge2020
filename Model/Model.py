9616593d5d22e51966b04d62968c7cbc82c98708
chungen1007 <en1007en@gmail.com>
2020-07-14 17:05:35 +0800

    Add player image timer

--- Model/Model.py
+++ Model/Model.py
@@ -82,6 +82,7 @@ class GameEngine:
         '''
         self.clock = pg.time.Clock()
         self.timer = Const.GAME_LENGTH
+        self.player_image_timer = 0 # only use for player image change, change when STATE_PLAY and STATE_ENDGAME
         self.item_amount = Const.ITEMS_INIT_AMOUNT
         self.generate_item_probability = Const.GENERATE_ITEM_PROBABILITY
         self.death_rain_emerge_time = random.randint(*Const.DEATH_RAIN_EMERGE_TIME_RANGE)
@@ -121,6 +122,7 @@ class GameEngine:
                 self.update_objects()
                 self.update_variable()
                 self.timer -= 1
+                self.player_image_timer += 1
                 # check if game ends
                 cnt = sum(player.is_alive() for player in self.players)
                 if self.timer == 0 or cnt <= 1:
@@ -128,6 +130,7 @@ class GameEngine:
             elif cur_state == Const.STATE_STOP:
                 self.update_stop()
             elif cur_state == Const.STATE_ENDGAME:
+                self.player_image_timer += 1
                 self.update_endgame()
 
         elif isinstance(event, EventPlay):
