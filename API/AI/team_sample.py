c6e93568351023618582016a8ddde128a46a00c1
Henry <b08902142@ntu.edu.tw>
2020-07-14 20:48:04 +0800

    add all AI in AL dir

---
+++ API/AI/team_sample.py
@@ -0,0 +1,30 @@
+from API.base import BaseAI
+
+# action
+AI_DIR_LEFT        = 0
+AI_DIR_RIGHT       = 1
+AI_DIR_JUMP        = 2
+AI_DIR_LEFT_JUMP   = 3
+AI_DIR_RIGHT_JUMP  = 4
+AI_DIR_ATTACK      = 5
+AI_DIR_USE_ITEM    = 6
+AI_DIR_STAY        = 7
+
+# item_id
+NO_ITEM = 0
+BANANA_PISTOL = 1
+BIG_BLACK_HOLE = 2
+CANCER_BOMB = 3
+ZAP_ZAP_ZAP = 4
+BANANA_PEEL = 5
+RAINBOW_GROUNDER = 6
+INVINCIBLE_BATTERY = 7
+
+class TeamAI(BaseAI):
+    def __init__(self, helper):
+        self.helper = helper
+        self.enhancement = [0, 0, 0, 0]
+    
+    def decide(self):
+        # add your code
+        pass
