77a1b6527e59834cfb00a0c0d7222371f9c52fc3
Henry <b08902142@ntu.edu.tw>
2020-07-19 21:01:19 +0800

    update henryAI

--- AI/team_henryAI.py
+++ AI/team_henryAI.py
@@ -1,5 +1,5 @@
-from API.base import BaseAI
-import Const
+
+
 AI_DIR_LEFT        = 0
 AI_DIR_RIGHT       = 1
 AI_DIR_JUMP        = 2
@@ -20,15 +20,17 @@ ZAP_ZAP_ZAP = 4
 BANANA_PEEL = 5
 RAINBOW_GROUNDER = 6
 INVINCIBLE_BATTERY = 7
+X_OFFSET = 34.5
+Y_OFFSET = 31.85
 
-class TeamAI(BaseAI):
+class TeamAI:
     def __init__(self, helper):
         self.helper = helper
         self.enhancement = [0, 0, 0, 0]
         self.searching_item = False
 
     def in_the_map(self, position):
-        if position[1] < 603 + Const.Y_OFFSET and position[0] >= 0 + Const.X_OFFSET and position[0] <= 1132 + Const.X_OFFSET:
+        if position[1] < 603 + Y_OFFSET and position[0] >= 0 + X_OFFSET and position[0] <= 1132 + X_OFFSET:
             return True
         return False
 
@@ -43,7 +45,7 @@ class TeamAI(BaseAI):
             self.searching_item = True
             return AI_DIR_ATTACK
         elif self.helper.get_self_keep_item_id() != NO_ITEM:
-            if self.helper.get_self_can_attack_time <= 0.2:
+            if self.helper.get_self_can_attack_time() <= 0.2:
                 self.searching_item = False
             return AI_DIR_USE_ITEM
         elif self.in_the_map(target_player_position) and self.helper.get_self_can_attack_time() <= 0.2 and not self.searching_item:
@@ -54,9 +56,9 @@ class TeamAI(BaseAI):
             elif self.helper.get_nearest_specific_item_position(6) != None:
                 return  self.helper.walk_to_position(self.helper.get_nearest_specific_item_position(6))
             elif self.helper.item_exists():
-                return self.helper.walk_to_position(self.helper.get_nearest_item_position)
+                return self.helper.walk_to_position(self.helper.get_nearest_item_position())
                 
-        platform_id = self.helper.get_above_which_land(self.helper.get_self_position())
+        platform_id = self.helper.get_above_which_platform(self.helper.get_self_position())
         if platform_id != -1:
             # go to middle of the platform
             platform_pos = self.helper.get_platform_position()[platform_id]
